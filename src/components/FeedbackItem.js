6045167bbd7056fb7724d7c3825cb42a37796b2f
Bobinho100 <114113147+Bobinho100@users.noreply.github.com>
2023-06-03 16:05:27 -0600

    Made the header component and the Feedbackitem

---
+++ src/components/FeedbackItem.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../styles/FeedbackItem.css'
+
+const FeedbackItem = () => {
+  return (
+    <>
+
+
+        <div className='card'>
+            <div className='num-display'> 10</div>
+            <div className='text-display'>This is an example of a feedback item</div>
+
+
+
+
+
+
+        </div>
+    
+    
+    
+    </>
+
+    
+  )
+}
+
+export default FeedbackItem
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/Header.css';
+
+const Header = () => {
+  return (
+
+    <>
+        <h1 className='header-title'>Feeback Ui</h1>
+    </>
+   
+
+  )
+}
+
+export default Header
