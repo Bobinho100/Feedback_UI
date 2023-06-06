9c67294516b3b088385915304afbbefb67a12683
Bobinho100 <114113147+Bobinho100@users.noreply.github.com>
2023-06-05 20:41:13 -0600

    Also worked on the button and rating to select

---
+++ src/shared/Button.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import '../styles/Button.css'
+
+const Button = ({children, version, type, isDisabled}) => {
+  return (
+    <button type={type}  disabled = {isDisabled}
+
+    className = {`btn btn-${version}`}
+    
+    
+    >
+
+
+
+    </button>
+  )
+}
+
+Button.defaultProps = {
+    version:'primary',
+    type: 'button',
+    isDisabled:false
+
+
+}
+
+
+
+export default Button
